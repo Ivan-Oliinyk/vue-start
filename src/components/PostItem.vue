ea7b86cee2c43bbfe1df41e7182c2b0668a7fc6d
Ivan <lorf1991@gmail.com>
2022-08-04 18:10:32 +0300

    work with components

---
+++ src/components/PostItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <li class="post">
+    <div><strong>Name: </strong>{{ post.title }}</div>
+    <div><strong>Description: </strong>{{ post.body }}</div>
+    <close-btn @click="$emit('remove', post)"/>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.post {
+  position: relative;
+  font-size: 1.6rem;
+  padding: 1.5rem;
+  border: none;
+  border-radius: 0.5rem;
+  background-color: lightgoldenrodyellow;
+  color: darkslateblue;
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h3 class="title">Post list</h3>
+    <ul class="post-container">
+      <post-item v-for="post in posts" :post="post" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+
+export default {
+  components: { PostItem },
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.title {
+  font-size: 2rem;
+  color: darkslateblue;
+}
+.post-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 2rem;
+}
+
+@media screen and (max-width: 1400px) {
+  .post-container {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media screen and (max-width: 992px) {
+  .post-container {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media screen and (max-width: 576px) {
+  .post-container {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
